d19843d39b5ee7113b0aaa8718386ca07071d6fb
lee970808@naver.com <lee970808@naver.com>
2023-04-27 21:43:58 +0900

    member 엔티티 관련 jpa test중,

--- src/main/java/com/kh/team4/entity/Member.java
+++ src/main/java/com/kh/team4/entity/Member.java
@@ -1,13 +1,16 @@
 package com.kh.team4.entity;
 
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-@Setter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "members")
 @SequenceGenerator(
         name = "member_seq_generator"  //시퀀스 제너레이터 이름
@@ -46,4 +49,16 @@ public class Member {
 
     @Column(columnDefinition = "varchar2(100)", nullable = false)
     private String address;
+
+    @Builder // 생성자 대신 이용하는 친구
+    public Member(String mtype, String mname, String regno, String mid, String pwd, String email, String ph, String address) {
+        this.mtype = mtype;
+        this.mname = mname;
+        this.regno = regno;
+        this.mid = mid;
+        this.pwd = pwd;
+        this.email = email;
+        this.ph = ph;
+        this.address = address;
+    }
 }
