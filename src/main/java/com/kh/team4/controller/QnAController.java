730e14a3457df77a12a25e6ee400e429da0221d8
sungjjun22 <sungjun2086@gmail.com>
2023-04-27 21:42:37 +0900

    DTO, ENTITY, SERVICE, CONTROLLER 추가 후 구현 중.

---
+++ src/main/java/com/kh/team4/controller/QnAController.java
@@ -0,0 +1,20 @@
+package com.kh.team4.controller;
+
+import com.kh.team4.service.QnAService;
+import com.kh.team4.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/qna")
+public class QnAController {
+/*    private final QnAService qnaService;
+
+    private final ReplyService replyService;
+
+    @GetMapping("/save")
+    public String saveForm() { return "/dashboard/QnA";}*/
+}
---
+++ src/main/java/com/kh/team4/dto/QnADTO.java
@@ -0,0 +1,21 @@
+package com.kh.team4.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString   // 필드 값 확인  시 사용
+@NoArgsConstructor  // 기본 생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
+public class QnADTO {
+    private Long qno;
+    private String title;
+    private String content;
+    private Integer secret;
+    private Integer hits;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+
+}
