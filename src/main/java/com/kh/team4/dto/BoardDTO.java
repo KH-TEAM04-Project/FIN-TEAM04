f910e4ce87321a738a2182c6f2275c0c6521aa1c
LAPTOP-GI206E4R\iamna <zhlozn0510@gmail.com>
2023-04-27 21:27:54 +0900

    boardDTO 생성

--- src/main/java/com/kh/team4/dto/BoardDTO.java
+++ src/main/java/com/kh/team4/dto/BoardDTO.java
@@ -1,4 +1,21 @@
 package com.kh.team4.dto;
 
+import lombok.*;
+import java.time.LocalDateTime;
+
+// DTO(Data Transfer Object), VO, Bean,
+// Entity - 다른 목적을 가진 클래스, 하나의 객체에 담아서 주고 받는 용도
+@Getter
+@Setter
+@ToString   // 필드 값 확인 시 사용
+@NoArgsConstructor  // 기본 생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
 public class BoardDTO {
-}
+    private Long bno;
+    private String title;
+    private String content;
+    private String secret;
+    private Integer hits;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+    }
