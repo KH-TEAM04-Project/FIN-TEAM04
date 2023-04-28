21dcbd9cf32160f399e00e4df44809fe30031e5d
DESKTOP-EIM4KQD\yskh9 <yskh95s@naver.com>
2023-04-28 21:06:48 +0900

    test 코드 작성

---
+++ src/main/java/com/kh/team4/test/Test.java
@@ -0,0 +1,13 @@
+package com.kh.team4.test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    private int number;
+    private String name;
+    private String nickname;
+
+}
