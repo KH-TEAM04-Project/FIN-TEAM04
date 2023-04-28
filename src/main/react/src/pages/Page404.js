d641c78f129993fa79943124680fdd246a63b881
DESKTOP-9S7QKKP <herk5861@naver.com>
2023-04-28 21:40:01 +0900

    Revert "게시판 글작성 페이지를 만들어 보았읍니다."
    This reverts commit f86299e8a099fa5e1bf15e462ef02e3c3a0dd64f.

--- src/main/react/src/pages/Page404.js
+++ src/main/react/src/pages/Page404.js
@@ -1,52 +1,52 @@
-import { Helmet } from 'react-helmet-async';
-import { Link as RouterLink } from 'react-router-dom';
-// @mui
-import { styled } from '@mui/material/styles';
-import { Button, Typography, Container, Box } from '@mui/material';
+// import { Helmet } from 'react-helmet-async';
+// import { Link as RouterLink } from 'react-router-dom';
+// // @mui
+// import { styled } from '@mui/material/styles';
+// import { Button, Typography, Container, Box } from '@mui/material';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-const StyledContent = styled('div')(({ theme }) => ({
-  maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  padding: theme.spacing(12, 0),
-}));
+// const StyledContent = styled('div')(({ theme }) => ({
+//   maxWidth: 480,
+//   margin: 'auto',
+//   minHeight: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   flexDirection: 'column',
+//   padding: theme.spacing(12, 0),
+// }));
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-export default function Page404() {
-  return (
-    <>
-      <Helmet>
-        <title> 404 Page Not Found | 꽁머니 </title>
-      </Helmet>
+// export default function Page404() {
+//   return (
+//     <>
+//       <Helmet>
+//         <title> 404 Page Not Found | 꽁머니 </title>
+//       </Helmet>
 
-      <Container>
-        <StyledContent sx={{ textAlign: 'center', alignItems: 'center' }}>
-          <Typography variant="h3" paragraph>
-            Sorry, page not found!
-          </Typography>
+//       <Container>
+//         <StyledContent sx={{ textAlign: 'center', alignItems: 'center' }}>
+//           <Typography variant="h3" paragraph>
+//             Sorry, page not found!
+//           </Typography>
 
-          <Typography sx={{ color: 'text.secondary' }}>
-            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check your
-            spelling.
-          </Typography>
+//           <Typography sx={{ color: 'text.secondary' }}>
+//             Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check your
+//             spelling.
+//           </Typography>
 
-          <Box
-            component="img"
-            src="/assets/illustrations/illustration_404.svg"
-            sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
-          />
+//           <Box
+//             component="img"
+//             src="/assets/illustrations/illustration_404.svg"
+//             sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
+//           />
 
-          <Button to="/" size="large" variant="contained" component={RouterLink}>
-           홈페이지로 돌아가기
-          </Button>
-        </StyledContent>
-      </Container>
-    </>
-  );
-}
+//           <Button to="/" size="large" variant="contained" component={RouterLink}>
+//            홈페이지로 돌아가기
+//           </Button>
+//         </StyledContent>
+//       </Container>
+//     </>
+//   );
+// }
