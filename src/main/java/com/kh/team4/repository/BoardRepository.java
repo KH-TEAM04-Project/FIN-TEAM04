05695c2668d599b950383d3a03b25b605e320c95
LAPTOP-GI206E4R\iamna <zhlozn0510@gmail.com>
2023-05-02 03:30:24 +0900

    재연/페이징 관련 DTO 생성

--- src/main/java/com/kh/team4/repository/BoardRepository.java
+++ src/main/java/com/kh/team4/repository/BoardRepository.java
@@ -1,4 +1,15 @@
 package com.kh.team4.repository;
 
-public interface BoardRepository {
+import com.kh.team4.dto.BoardDTO;
+import com.kh.team4.dto.PageResultDTO;
+import com.kh.team4.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Repository인터페이스 : Entity의 기본적인 CRUD 가능하도록 함
+// JpaRepository<엔티티명, ID타입> 인터페이스 상속받으면 @Repository 추가X
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+
+
 }
+
