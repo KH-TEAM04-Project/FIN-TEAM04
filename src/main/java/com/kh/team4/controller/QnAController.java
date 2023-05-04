cc9306c674634a5537d5bb6a0195041be6cff883
sungjjun22 <sungjun2086@gmail.com>
2023-05-04 15:29:11 +0900

    Qna 인설트 완성 후 테스트코드 작성 중 풀받으려고 퓌쉬

--- src/main/java/com/kh/team4/controller/QnAController.java
+++ src/main/java/com/kh/team4/controller/QnAController.java
@@ -3,27 +3,31 @@ package com.kh.team4.controller;
 import com.kh.team4.dto.QnaDTO;
 import com.kh.team4.service.QnaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/dashboard")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class QnAController {
     private final QnaService qnaService;
 
-    @GetMapping("save")
-    public List<String> home() {
-        return Arrays.asList("글쓰기 페이지입니다.");
+    @PostMapping("/qna")
+    public ResponseEntity<String> register(@RequestBody QnaDTO qnaDTO, @RequestParam Long mno) {
+        // "/dashboard/write" 주소로 POST 요청이 들어오면 QnaDTO를 받아서 QnaService의 register() 메서드를 호출
+        qnaService.register(qnaDTO, mno);
+        // register() 메서드에서는 QnaDTO를 이용하여 게시글을 작성하고, Qna 객체를 데이터베이스에 저장.
+        return new ResponseEntity<>("게시글이 작성되었습니다.", HttpStatus.OK);
+        // 성공적으로 수행되면 "게시글이 작성되었습니다" 메시지와 함께 HttpStatus.OK(200) 상태 코드를 반환.
     }
-    @PostMapping("save")
-    public String save(QnaDTO qnaDTO) throws IOException {
-        System.out.println("qnaDTO = " + qnaDTO);
-        qnaService.save(qnaDTO);
-        return "/dashboard/QnA";
+    @GetMapping("/qna")
+    public List<String> register() {
+        return Arrays.asList("qna 페이지입니다");
     }
 }
