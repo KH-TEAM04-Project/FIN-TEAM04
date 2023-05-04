ba0338d0f97d779475899d5d1219767c10f54095
DESKTOP-24KK5MP\administer <tjrwns4824@naver.com>
2023-05-04 21:17:43 +0900

    컬럼명 변경

--- src/main/java/com/kh/team4/test/SignUpForm.java
+++ src/main/java/com/kh/team4/test/SignUpForm.java
@@ -1,10 +1,12 @@
 package com.kh.team4.test;
 
 public class SignUpForm {
-    private String fullname;
-    private String id;
-    private String ssn;
+    private String mname ;
+    private String mid;
+    private String regno;
     private String email;
-    private String password;
-    private String confirmPassword;
+    private String pwd;
+    private String ph;
+    private String detailaddress;
+    private String address;
 }
