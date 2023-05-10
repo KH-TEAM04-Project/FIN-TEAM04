ed9dcbd9067836d43481f100b29cbe3ee63ed7c7
DESKTOP-9S7QKKP <herk5861@naver.com>
2023-05-10 15:33:28 +0900

    수정

--- src/main/react/src/pages/EoardPage.js
+++ src/main/react/src/pages/EoardPage.js
@@ -106,7 +106,7 @@ const [open1, setOpen1] = React.useState(false);
                       <Link to={`/BoardReadPage/${data.bno}`}>{data.title}</Link>
                     </TableCell>
                     <TableCell align="right">{data.content}</TableCell>
-                    <TableCell align="right">{data.writer}</TableCell>
+                    <TableCell align="right">작성자들</TableCell>
                     <TableCell align="right">{data.regDate}</TableCell>
                     <TableCell align="right">{data.hits}</TableCell>
                     <TableCell align="right">
@@ -128,7 +128,7 @@ const [open1, setOpen1] = React.useState(false);
                       <p id="parent-modal-description">
                         진짜 글삭제 되는데요?
                       </p>
-                      <Button href="http://localhost:3000/re" onClick={() => handleDelete(data.qno)}>진짜 삭제</Button>
+                      <Button href="http://localhost:3000/EoardPage" onClick={() => handleDelete(data.qno)}>진짜 삭제</Button>
                     </Box>
                      </Modal>
                       </TableCell>
