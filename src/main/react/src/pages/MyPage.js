6ae6a4a482fcb6addf4a6db49897f059ebcb3db7
DESKTOP-24KK5MP\administer <tjrwns4824@naver.com>
2023-05-22 19:06:22 +0900

    로그인시 메인페이지에서 로그인으로가는 아이콘 숨기기 - o 로그인시 생성되는 쿠키 회원탈퇴할때 같이 삭제되게끔 설정 - 0 마이페이지에서 회원정보수정란 정리  - 0

--- src/main/react/src/pages/MyPage.js
+++ src/main/react/src/pages/MyPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import MyPageForm from "../sections/auth/MyPage/MyPageForm";
+import { useNavigate } from "react-router-dom";
 
 function MyPage() {
   const [userData, setUserData] = useState({
@@ -10,10 +10,12 @@ function MyPage() {
     email: "",
     address: "",
     detailAddress: "",
-    ph: ""
+    ph: "",
+    pwd: "" // Include pwd in the initial state
   });
-
+  const navigate = useNavigate();
   const [mno, setMno] = useState(""); // 토큰에서 추출한 sub 값 상태
+  const [isEditing, setIsEditing] = useState(false); // 회원 정보 수정 여부 상태
 
   // 로컬 스토리지에서 토큰 값을 가져옴
   const token = localStorage.getItem('accessToken');
@@ -53,10 +55,53 @@ function MyPage() {
     return regno;
   };
 
-  // Function to handle saving the updated user information and changing the password
-  const handleSaveAndChangePassword = () => {
-    // Implement the logic to handle saving user information and changing the password
-    console.log("Save and Change Password logic goes here");
+  // Function to handle updating the user information
+  const handleUpdateMember = (e) => {
+    e.preventDefault();
+
+    // Implement the logic to handle updating user information
+    const { email, pwd, detailAddress, address, ph } = userData;
+
+    // Create a MemberReqDTO object with the updated information
+    const memberReqDTO = {
+      mno, // Include the mno value
+      email,
+      pwd,
+      detailAddress,
+      address,
+      ph
+    };
+
+    // Send the updated user information to the backend
+    axios.post("/memberUpdate", memberReqDTO)
+      .then(response => {
+        // Successful update
+        const updatedUserData = response.data;
+
+        if (updatedUserData) {
+          // Update the user data in the state
+          setUserData(updatedUserData);
+          setIsEditing(false); // Exit editing mode
+          console.log("User information updated successfully");
+          alert("사용자 정보가 성공적으로 업데이트되었습니다.");
+        } else {
+          console.log("Failed to update user information");
+          alert("사용자 정보 업데이트에 실패했습니다.");
+        }
+      })
+      .catch(error => {
+        // Error occurred while updating
+        console.error(error);
+        alert("사용자 정보 업데이트 중 에러가 발생했습니다.");
+      });
+  };
+
+  // Function to handle changes in the input fields
+  const handleChange = (field, value) => {
+    setUserData(prevState => ({
+      ...prevState,
+      [field]: value
+    }));
   };
 
   // 회원 삭제 함수
@@ -72,9 +117,12 @@ function MyPage() {
           email: "",
           address: "",
           detailAddress: "",
-          ph: ""
+          ph: "",
+          pwd: "" // Include pwd in the initial state
         });
         alert("회원 정보가 삭제되었습니다.");
+        document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        navigate("/login");
       })
       .catch(error => {
         // Error occurred while deleting
@@ -82,29 +130,75 @@ function MyPage() {
       });
   };
 
-  // Function to handle changes in the input fields
-  const handleChange = (field, value) => {
-    setUserData(prevState => ({
-      ...prevState,
-      [field]: value
-    }));
-  };
-
   return (
     <div>
       <h1>마이페이지</h1>
-      <p>이름: {userData.mname}</p>
-      <p>아이디: {userData.mid}</p>
-      <p>생년월일: {getMaskedRegNo()}</p>
-      <p>이메일: {userData.email}</p>
-      <p>주소: {userData.address}</p>
-      <p>상세주소: {userData.detailAddress}</p>
-      <p>휴대폰번호: {userData.ph}</p>
+      {userData.mname && !isEditing && (
+        <>
+          <p>이름: {userData.mname}</p>
+          <p>아이디: {userData.mid}</p>
+          <p>생년월일: {getMaskedRegNo()}</p>
+          <p>이메일: {userData.email}</p>
+          <p>주소: {userData.address}</p>
+          <p>상세주소: {userData.detailAddress}</p>
+          <p>휴대폰번호: {userData.ph}</p>
+        </>
+      )}
 
-      <MyPageForm onSave={handleChange} />
+      {isEditing && (
+        <form>
+          <div>
+            <label htmlFor="email">이메일</label>
+            <input
+              type="text"
+              id="email"
+              value={userData.email}
+              onChange={(e) => handleChange("email", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="pwd">패스워드</label>
+            <input
+              type="password"
+              id="pwd"
+              value={userData.pwd}
+              onChange={(e) => handleChange("pwd", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="detailaddress">상세주소</label>
+            <input
+              type="text"
+              id="detailaddress"
+              value={userData.detailAddress}
+              onChange={(e) => handleChange("detailAddress", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="address">주소</label>
+            <input
+              type="text"
+              id="address"
+              value={userData.address}
+              onChange={(e) => handleChange("address", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="ph">휴대폰번호</label>
+            <input
+              type="text"
+              id="ph"
+              value={userData.ph}
+              onChange={(e) => handleChange("ph", e.target.value)}
+            />
+          </div>
+          <button onClick={handleUpdateMember}>회원 정보 수정</button>
+        </form>
+      )}
 
-      {/* 수정 및 비밀번호 변경 버튼 */}
-      <button onClick={handleSaveAndChangePassword}>회원 정보 수정</button>
+      {userData.mname && !isEditing && (
+        <button onClick={() => setIsEditing(true)}>회원 정보 수정</button>
+      )}
 
       {/* 회원 삭제 버튼 */}
       <button onClick={handleDelete}>회원 삭제</button>
