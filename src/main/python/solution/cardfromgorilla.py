aa1af99310af0f94e9637cdf38773bd927625851
lee970808@naver.com <lee970808@naver.com>
2023-06-15 16:47:38 +0900

    파이썬으로 스크래핑한 데이터 json형태로 변환 후 spring으로 송신 적용 완.

--- src/main/python/solution/cardfromgorilla.py
+++ src/main/python/solution/cardfromgorilla.py
@@ -9,34 +9,35 @@ def scraping():
 
     driver = webdriver.Chrome()
 
-    # url = "https://www.card-gorilla.com/chart/check100?term=weekly"
+    url = "https://www.card-gorilla.com/chart/check100?term=weekly"
 
-    # driver.get(url)
-    # html = driver.page_source
-    # soup = BeautifulSoup(html, 'html.parser')
-    # sele = soup.select('div.card_img>p>a')
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    sele = soup.select('div.card_img>p>a')
 
-    # # num = 0
-    # cardNumList = np.array([])
+    # num = 0
+    cardNumList = np.array([])
 
-    # for a in sele:
-    #     href = a.attrs['href'][13:]
-    #     # print(num, href)
-    #     # num = num + 1
-    #     cardNumList = np.append(cardNumList, href)
+    for a in sele:
+        href = a.attrs['href'][13:]
+        # print(num, href)
+        # num = num + 1
+        cardNumList = np.append(cardNumList, href)
 
-    # cardNumList = np.delete(cardNumList, 0)
+    cardNumList = np.delete(cardNumList, 0)
 
-    # print(cardNumList)
-    # # 카드 번호 추출 완료.
+    print(cardNumList)
+    # 카드 번호 추출 완료.
 
     # 테스트용
-    cardNumList = np.array([435, 618, 646, 2432])
+    # cardNumList = [435, 618, 646, 2432]
 
 
     detail = "https://www.card-gorilla.com/card/detail/"
 
-    cardInfoList = np.empty((0, 9), str)
+    cardInfoList =[]
+    cardInfoList2 =[]
 
     for a in cardNumList:
         detailurl = detail + str(a)
@@ -55,7 +56,7 @@ def scraping():
         if soup.select_one('div.bnf1>dl.bnf11>dt') == None:
             bename1 = ''
             beval1 = ''
-        else : 
+        else :
             bename1 = soup.select_one('div.bnf1>dl.bnf11>dt').text
             beval1 = soup.select_one('div.bnf1>dl.bnf11>dd>strong').text
 
@@ -63,7 +64,7 @@ def scraping():
         if soup.select_one('div.bnf1>dl.bnf12>dt') == None:
             bename2 = ''
             beval2 = ''
-        else : 
+        else :
             bename2 = soup.select_one('div.bnf1>dl.bnf12>dt').text
             beval2 = soup.select_one('div.bnf1>dl.bnf12>dd>strong').text
 
@@ -74,11 +75,35 @@ def scraping():
         else :
             bename3 = soup.select_one('div.bnf1>dl.bnf13>dt').text
             beval3 = soup.select_one('div.bnf1>dl.bnf13>dd>strong').text
-        
+
 
         cardinfo = [brand, name, img, bename1, beval1, bename2, beval2, bename3, beval3]
-        cardInfoList = np.append(cardInfoList, [cardinfo], axis=0)
-        
+
+        cardInfoList.append({"카드사" : cardinfo[0], "카드명" : cardinfo[1], "카드이미지" : cardinfo[2], "혜택1" : cardinfo[3], "혜택1내용" : cardinfo[4], "혜택2" : cardinfo[5], "혜택2내용" : cardinfo[6], "혜택3" : cardinfo[7], "혜택3내용" : cardinfo[8]})
+
+
+    card_info_dict = {}
+
+
+    # Convert the cardInfoList2 to JSON
+    json_data = json.dumps(cardInfoList)
     print(cardInfoList)
+    # Write the JSON data to a file
+    """ with open('card_info.json', 'w') as file:
+        file.write(json_data)
+
+    # print(cardInfoList)
+    print(json_data) """
+
+    for item in cardInfoList:
+        print(item)
+
+    json_data2 = json.dumps(item)
+
+    # Write the JSON data to a file
+    """ with open('card_info2.json', 'w') as file:
+        file.write(json_data2)
+    print(json_data2) """
+
 
     return cardInfoList
