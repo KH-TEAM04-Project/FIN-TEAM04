de331d902e5494612d0e28e826f344eca9a60721
sungjjun22 <sungjun2086@gmail.com>
2023-06-21 16:02:41 +0900

    좋아요 기능 구현 완료 //// 취소 기능 구현 중

--- src/main/java/com/kh/team4/repository/LikesRepository.java
+++ src/main/java/com/kh/team4/repository/LikesRepository.java
@@ -1,10 +1,16 @@
 package com.kh.team4.repository;
 
 import com.kh.team4.entity.Likes;
+import com.kh.team4.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-    List<Likes> findAllByQnoAndMno(Long qno, Long mno);
+
+    int countByQnaQno(Long qno);
+
+    List<Likes> findAllByQnaQnoAndMemberMno(Long qno, Long mno);
+
+    List<Likes> findByQnaQno(Long qno);
 }
